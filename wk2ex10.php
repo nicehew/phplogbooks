9895b04f353427386159cbcb136bd3051e4142bf
nicehew <hew_is_better_than_hans@hotmail.com>
2019-02-22 01:03:10 +0000

    wk2fin

--- wk2ex10.php
+++ wk2ex10.php
@@ -2,10 +2,18 @@
   $mymarks["year 1"] = 55;
   $mymarks["year 2"] = 65;
   $mymarks["year 3"] = 75;
-
-  while(list($index,$value) = each($mymarks))
-  {
-    echo "for  $index  my grade was  $value <br/>";
-  }
+  ?>
+<body>
+<table border=1 align="centre">
+<tr><th>Year</th><th>Marks</th></tr>
+<?php
+while(list($index,$value) = each($mymarks))
+{
+  echo "<tr><td> $index </td><td> $value </td></tr> <br/>";
+}
+?>
+</table>
+</body>
+  <?php
   echo "<br/>My best year was Year 3 when I averaged ". $mymarks["year 3"];
 ?>
